62bb34f6babb5ca8a391e35efeedd1f263a47ab9
Duy Tu <duytu89@gmail.com>
2023-06-20 12:31:49 +0700

    dev

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { LogDebug } from './common/helper/debugLog';
+import { json } from 'stream/consumers';
+import { urlencoded } from 'express';
+import * as bodyParser from 'body-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(bodyParser.json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
   await app.listen(8090);
   console.log('http://127.0.0.1:3000/');
   LogDebug._info('sdfsdfdsfdsfd');
